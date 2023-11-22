6a49b12557038ea43d2b42bb499298d18a907399
Rino Brøndum <rinobroendum@gmail.com>
2023-11-22 09:41:01 +0100

    init

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       // creats 5 users + 2 listings with hard coded data
+       \App\Models\User::factory(5)->create();
+
+       \App\Models\Listing::create(
+         
+               [
+                   // title/name - heading
+                   'title' => 'title1',
+                   // company - subtitle
+                   'name' => 'name-user1',
+                   'location' => 'Boston, MA',
+                   'email' => 'email1@email.com',
+                   'tags' => 'female, straight, 25-30',
+                   'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+               ]
+           );
+
+           \App\Models\Listing::create(
+               [
+                   'name' => 'name-user2',
+                   'title' => 'title2',
+                   'location' => 'New York, NY',
+                   'email' => 'email2@email.com',
+                   'tags' => 'female, bi, 28-35',
+                   'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+               ],
+           );
+
+
+    }
+}
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+
+@section('content')
+@include('partials._hero')
+@include('partials._search')
+    
+
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+@if(count($listings)== 0)
+<p>No listings found</p>
+@endif
+
+@foreach($listings as $listing)
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    <div class="flex">
+      <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="" />
+      <div>
+        <h3 class="text-2xl">
+          <!-- NAME -->
+          <a href="/listings/{{$listing->id}}">{{$listing->name}}</a>
+        </h3>
+        <!-- TITLE-->
+        <div class="text-xl font-bold mb-4">{{$listing->title}}</div>
+         <!-- TAGS-->
+        <ul class="flex">
+          <li
+            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+          >
+            <a href="#">female</a>
+          </li>
+          <li
+            class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+          >
+            <a href="#">straight</a>
+          </li>
+        </ul>
+        <!-- Location-->
+        <div class="text-lg mt-4"><i class="fa-solid fa-location-dot"></i> {{$listing->location}}</div>
+      </div>
+    </div>
+  </div>
+@endforeach
+
+</div>
+
+@endsection
