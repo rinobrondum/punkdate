5f8bd449d367548a998ba8e7d03072e03504e0c5
Rino Brøndum <rinobroendum@gmail.com>
2023-12-03 17:42:55 +0100

    Tag filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -10,9 +10,8 @@ class ListingController extends Controller
 {
     // Show all listings
     public function index() {
-
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag']))->get()
                 
         ]);
     }
