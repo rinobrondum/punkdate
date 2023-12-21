3d34334caae82bf747a7e48a41e6f5d17542b4c3
Rino Brøndum <rinobroendum@gmail.com>
2023-12-21 15:07:39 +0100

    User Registration - felj med rule  under UserController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Listing;
 
 // data is coming from the model
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
@@ -45,3 +46,9 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
