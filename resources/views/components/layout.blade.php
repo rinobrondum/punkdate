1dfeb3cce2d90f18c83ecca84dca9b868531bf3a
Rino Brøndum <rinobroendum@gmail.com>
2023-12-21 18:25:39 +0100

    Auth Links

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -31,6 +31,18 @@
     <nav class="flex justify-between items-center mb-4">
       <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
       <ul class="flex space-x-6 mr-6 text-lg">
+        @auth
+        <li>
+          <span class="font-bold uppercase">
+            Welcome {{auth()->user()->name}}
+          </span>
+        </li>
+        <li>
+          <a href="login/listings/manage" class="hover:text-laravel"
+            ><i class="fa-solid fa-gear"></i> Manage listings</a
+          >
+        </li>
+        @else
         <li>
           <a href="register" class="hover:text-laravel"
             ><i class="fa-solid fa-user-plus"></i> Register</a
@@ -38,9 +50,9 @@
         </li>
         <li>
           <a href="login" class="hover:text-laravel"
-            ><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a
-          >
+            ><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
         </li>
+        @endauth
       </ul>
     </nav>
 <main>
